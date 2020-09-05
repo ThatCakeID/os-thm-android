e28b31f2f88d3402349e6bbb6fae2a1ae1e23a34
Iyxan23 <nurihsanalghifari@gmail.com>
2020-09-05 14:19:33 +0700

    Added new Constructors and Added javadoc support

--- lib/src/main/java/tw/osthm/Theme.java
+++ lib/src/main/java/tw/osthm/Theme.java
@@ -3,8 +3,19 @@ package tw.osthm;
 import android.content.Context;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.util.HashMap;
+
+/**
+ * <h1>Theme object model</h1>
+ * This theme object model is used to
+ * represent a theme in an object
+ *
+ * @author Iyxan23
+ * @since 2020
+ * @version 1.0
+ */
 
 public class Theme {
     Context mContext;
@@ -22,7 +33,7 @@ public class Theme {
     public int colorPrimaryTint = -1;
     public int colorBackgroundTint = -14575885;
     public int colorPrimaryCard = -1;
-    public int colorBackgroundCard;
+    public int colorBackgroundCard = 0xFFFFFF;
     public int colorPrimaryCardText = -16777216;
     public int colorBackgroundCardText = -16777216;
     public int colorPrimaryCardTint = -16777216;
@@ -33,10 +44,66 @@ public class Theme {
     public String themesauthor;
     public int themeversion;
 
+    /**
+     * Constructor with JSON as String
+     * @param mContext Context
+     * @param json_string JSON as String
+     */
+    public Theme(Context mContext, String json_string) {
+        this.mContext = mContext;
+        HashMap<String, Object> json_data = new Gson().fromJson(
+                json_string, new TypeToken<HashMap<String, Object>>() {}.getType()
+        );
+
+        this.setData(json_data);
+    }
+
+    /**
+     * Constructor with HashMap as data
+     * @param mContext Context
+     * @param data Theme Data
+     */
+    public Theme(Context mContext, HashMap<String, Object> data) {
+        this.mContext = mContext;
+
+        this.setData(data);
+    }
+
+    /**
+     * Constructor with default theme
+     * @param mContext Context
+     */
     public Theme(Context mContext) {
         this.mContext = mContext;
     }
 
+    /**
+     * Constructor with manually picked colors
+     * @param mContext Context
+     * @param colorPrimary Primary Color
+     * @param colorPrimaryText Primary Text Color
+     * @param colorPrimaryDark Primary Dark Color
+     * @param colorStatusbarTint Statusbar Color
+     * @param colorBackground Background color for root
+     * @param colorBackgroundText Background color for text
+     * @param colorAccent Color Accent
+     * @param colorAccentText Color Accent for text
+     * @param shadow Is shadow enabled
+     * @param colorControlHighlight Color on highlight
+     * @param colorHint Color Hint for EditText
+     * @param colorPrimaryTint Imageview tint color
+     * @param colorBackgroundTint Background Tint color
+     * @param colorPrimaryCard Card Color
+     * @param colorBackgroundCard Card Background Color
+     * @param colorPrimaryCardText Color for Text on card
+     * @param colorBackgroundCardText Color for Text Background on card
+     * @param colorPrimaryCardTint Tint for imageview on card
+     * @param colorBackgroundCardTint Background color for card tint
+     * @param themesname Theme name
+     * @param themesinfo Theme info/ description
+     * @param themesauthor Theme Author
+     * @param themeversion Theme version
+     */
     public Theme(Context mContext, int colorPrimary, int colorPrimaryText,
                  int colorPrimaryDark, int colorStatusbarTint, int colorBackground,
                  int colorBackgroundText, int colorAccent, int colorAccentText,
@@ -73,7 +140,42 @@ public class Theme {
         this.themesauthor = themesauthor;
         this.themeversion = themeversion;
     }
-    
+
+    /**
+     * Set theme using HashMap
+     * @param data Theme HashMap
+     */
+    public void setData(HashMap<String, Object> data) {
+        this.colorPrimary = (int) data.get("colorPrimary");
+        this.colorPrimaryText = (int) data.get("colorPrimaryText");
+        this.colorPrimaryDark = (int) data.get("colorPrimaryDark");
+        this.colorStatusbarTint = (int) data.get("colorStatusbarTint");
+        this.colorBackground = (int) data.get("colorBackground");
+        this.colorBackgroundText = (int) data.get("colorBackgroundText");
+        this.colorAccent = (int) data.get("colorAccent");
+        this.colorAccentText = (int) data.get("colorAccentText");
+        this.shadow = (int) data.get("shadow");
+        this.colorControlHighlight = (int) data.get("colorControlHighlight");
+        this.colorHint = (int) data.get("colorHint");
+        this.colorPrimaryTint = (int) data.get("colorPrimaryTint");
+        this.colorBackgroundTint = (int) data.get("colorBackgroundTint");
+        this.colorPrimaryCard = (int) data.get("colorPrimaryCard");
+        this.colorBackgroundCard = (int) data.get("colorBackgroundCard");
+        this.colorPrimaryCardText = (int) data.get("colorPrimaryCardText");
+        this.colorBackgroundCardText = (int) data.get("colorBackgroundCardText");
+        this.colorPrimaryCardTint = (int) data.get("colorPrimaryCardTint");
+        this.colorBackgroundCardTint = (int) data.get("colorBackgroundCardTint");
+        this.themesname = (String) data.get("themesname");
+        this.themesinfo = (String) data.get("themesinfo");
+        this.themesauthor = (String) data.get("themesauthor");
+        this.themeversion = (int) data.get("themeversion");
+    }
+
+    /**
+     * This method is used to convert
+     * this theme into a HashMap
+     * @return Theme HashMap
+     */
     public HashMap<String, Object> toHashMap() {
         HashMap<String, Object> result = new HashMap<>();
         result.put("colorPrimary", colorPrimary);
@@ -102,6 +204,11 @@ public class Theme {
         return result;
     }
 
+    /**
+     * Convert this theme into a
+     * json string
+     * @return JSON String
+     */
     public String toJsonString() {
         /*  // Easier Version:
             HashMap<String, Object> result = this.toHashMap();
