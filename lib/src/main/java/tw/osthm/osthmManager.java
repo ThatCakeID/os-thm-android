e3b0bcb5b17e012975a456515f1393edd6eb9122
Iyxan23 <nurihsanalghifari@gmail.com>
2020-09-15 11:35:55 +0700

    Unfinished changes

--- lib/src/main/java/tw/osthm/osthmManager.java
+++ lib/src/main/java/tw/osthm/osthmManager.java
@@ -1,5 +1,22 @@
 package tw.osthm;
 
+import android.os.Environment;
+
 public class osthmManager {
-    
+
+    public static final String externalDir = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+    public static void init() {
+        // Initialize
+
+        // Check if .osthm folder exist
+        if (!StorageUtil.isDirectoryExists(externalDir + "/.osthm/")) {
+            // Initialize the folder structure
+            // Create the folder .osthm and .osthm/themes
+            StorageUtil.createDirectory(externalDir + "/.osthm/themes");
+
+            // Create the config file
+            StorageUtil.createFile(externalDir + "/.osthm/conf", "");
+        }
+    }
 }
