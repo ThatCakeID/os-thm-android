389113849fd4b652b9f628eb7fb48fed5efa27f9
ryenyuku <teamworks1732@gmail.com>
2020-09-16 21:31:17 +0700

    Some changes

--- lib/src/main/java/tw/osthm/osthmManager.java
+++ lib/src/main/java/tw/osthm/osthmManager.java
@@ -1,7 +1,11 @@
 package tw.osthm;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Environment;
+
+import androidx.core.content.ContextCompat;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -10,11 +14,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.UUID;
 
 public class osthmManager {
@@ -23,8 +24,14 @@ public class osthmManager {
     public static final String themes_folder = externalDir + "/.osthm/themes/";
     public static final String config_file = externalDir + "/.osthm/conf";
 
-    public static void init() {
+    public static void init(Context mContext) throws IOException {
         // Initialize
+
+        // Check permission
+        if (ContextCompat.checkSelfPermission(mContext, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(mContext,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            throw new IOException("Permission isn't granted!");
 
         // Check if .osthm folder exist
         if (!StorageUtil.isDirectoryExists(themes_folder) ||
@@ -39,31 +46,40 @@ public class osthmManager {
     }
 
     public static void setConf(String key, String value) throws IOException {
-        HashMap<String, String> data = parseConfJson();
+        HashMap<String, String> data = loadConfJson();
         data.put(key, value);
         writeConfJson(data);
     }
 
     public static String getConf(String key, String defaultValue) throws IOException {
-        HashMap<String, String> data = parseConfJson();
+        HashMap<String, String> data = loadConfJson();
         return data.containsKey(key) ? data.get(key) : defaultValue;
     }
 
     public static boolean containsConf(String key) throws IOException {
-        HashMap<String, String> data = parseConfJson();
+        HashMap<String, String> data = loadConfJson();
         return data.containsKey(key);
     }
 
-    //public static void setTheme()
+    public static void setTheme(String json) {
+
+    }
 
     // Utilities ===================================================================================
-    private static HashMap<String, String> parseConfJson() throws IOException {
+    private static HashMap<String, String> loadConfJson() throws IOException {
         return new Gson().fromJson(StorageUtil.readFile(config_file),
                 new TypeToken<HashMap<String, Object>>(){}.getType());
     }
 
     private static void writeConfJson(HashMap<String, String> confhashmap) {
         StorageUtil.appendFile(config_file, new Gson().toJson(confhashmap));
+    }
+
+    private static void writeTheme(String json, String UUID) {
+        if (StorageUtil.isFileExist(themes_folder + UUID + ".os-thm"))
+            StorageUtil.appendFile(themes_folder + UUID + ".os-thm", json);
+        else
+            StorageUtil.createFile(themes_folder + UUID + ".os-thm", json);
     }
 
     public static boolean isJSONValid(String test) {
