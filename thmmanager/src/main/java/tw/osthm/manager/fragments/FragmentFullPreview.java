6b6fa87f9108c26f3ee99af0759e171d5b6bc035
Iyxan23 <nurihsanalghifari@gmail.com>
2020-09-18 14:00:45 +0700

    Revert "Revert "Added Full Preview Fragent""
    This reverts commit 084dfc6dbbbd2a9c1d7d02049fe61d3562f95df5.

---
+++ thmmanager/src/main/java/tw/osthm/manager/fragments/FragmentFullPreview.java
@@ -0,0 +1,29 @@
+package tw.osthm.manager.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import tw.osthm.manager.R;
+
+public class FragmentFullPreview extends Fragment {
+
+    public FragmentFullPreview() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_full_preview, container, false);
+    }
+}
