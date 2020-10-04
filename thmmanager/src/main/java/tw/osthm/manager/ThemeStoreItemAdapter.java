a53451ecf10bf0cfc56410e68afbadc9e566f8a4
Iyxan23 <nurihsanalghifari@gmail.com>
2020-10-04 18:31:58 +0700

    Finished the Adapter (untested)

--- thmmanager/src/main/java/tw/osthm/manager/ThemeStoreItemAdapter.java
+++ thmmanager/src/main/java/tw/osthm/manager/ThemeStoreItemAdapter.java
@@ -1,6 +1,8 @@
 package tw.osthm.manager;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.ColorStateList;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,9 +22,11 @@ public class ThemeStoreItemAdapter extends RecyclerView.Adapter<ThemeStoreItemAd
     private static final String TAG = "ThemeStoreItemAdapter";
 
     JSONArray datas;
+    Context mContext;
 
-    public ThemeStoreItemAdapter(JSONArray datas) {
+    public ThemeStoreItemAdapter(JSONArray datas, Context mContext) {
         this.datas = datas;
+        this.mContext = mContext;
     }
 
     public void updateView(JSONArray datas) {
@@ -41,11 +45,34 @@ public class ThemeStoreItemAdapter extends RecyclerView.Adapter<ThemeStoreItemAd
     @Override
     public void onBindViewHolder(@NonNull final ViewHolder holder, final int position) {
         Log.d(TAG, "onBindViewHolder: called.");
+
         JSONObject theme;
         JSONObject themedata;
+
+        holder.indicator.setVisibility(View.INVISIBLE);
+
         try {
             theme = new JSONObject(datas.getString(position));
             themedata = new JSONObject(theme.getString("themesjson"));
+        } catch (JSONException e) {
+            return;
+        }
+
+        try {
+            holder.theme_name.setText(themedata.getString("themesname"));
+            holder.theme_author.setText(themedata.getString("themesauthor"));
+    
+            holder.textview_name.setTextColor(themedata.getInt("colorBackgroundText"));
+            holder.textview_title.setTextColor(themedata.getInt("colorPrimaryText"));
+            holder.linear_base.setBackgroundTintList(ColorStateList.valueOf(themedata.getInt("colorBackground")));
+            holder.linear_title.setBackgroundTintList(ColorStateList.valueOf(themedata.getInt("colorPrimary")));
+            holder.imageview_fab.setBackgroundTintList(ColorStateList.valueOf(themedata.getInt("colorAccent")));
+            holder.imageview_fab.setColorFilter(themedata.getInt("colorAccentText"));
+            
+            holder.imageview_back.setColorFilter(themedata.getInt("colorPrimaryTint"));
+    
+            holder.linear_title.setElevation(themedata.getInt("shadow") == 1 ? ThmMgrUtils.toDip(mContext, 5f) : 0f);
+            holder.imageview_fab.setElevation(themedata.getInt("shadow") == 1 ? ThmMgrUtils.toDip(mContext, 6f) : 0f);
         } catch (JSONException e) {
             e.printStackTrace();
         }
