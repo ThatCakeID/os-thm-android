91729af7bc6d790a53ffd11f697198e15131a399
Iyxan23 <nurihsanalghifari@gmail.com>
2020-12-15 14:46:11 +0700

    Added OsThmTheme and OsThmMetadata support to osthmFile and valid JSON checking

--- lib/src/main/java/tw/osthm/OsThmMetadata.java
+++ lib/src/main/java/tw/osthm/OsThmMetadata.java
@@ -1,8 +1,42 @@
 package tw.osthm;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
+import android.view.Display;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 
 /**
@@ -46,5 +80,17 @@ public class OsThmMetadata {
         os_thm_version = (int) data.get("os-thm-version");
         uuid = data.get("uuid").toString();
         themeversion = (int) data.get("theme-version");
+    }
+
+    public HashMap<String, Object> toHashMap() {
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("themesname", themesname);
+        data.put("themesinfo", themesinfo);
+        data.put("themesauthor", themesauthor);
+        data.put("os-thm-version", os_thm_version);
+        data.put("uuid", uuid);
+        data.put("theme-version", themeversion);
+
+        return data;
     }
 }
--- lib/src/main/java/tw/osthm/osthmFile.java
+++ lib/src/main/java/tw/osthm/osthmFile.java
@@ -1,5 +1,11 @@
 package tw.osthm;
 
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -16,6 +22,10 @@ import java.util.HashMap;
  *
  * @author Iyxan23
  */
+
+
+// INFO: This class only returns and only receives full JSON Theme data
+
 
 public class osthmFile {
 
@@ -60,11 +70,48 @@ public class osthmFile {
         this.data = new JSONObject(data);
         return this;
     }
+
+    public osthmFile setData(OsThmTheme theme) {
+        try {
+            data.put("themesjson", theme.toHashMap());
+        } catch (JSONException e) {
+            Log.e("osthmFile", "Failed to set data as OsThmTheme (tw/osthm/osthmFile.java:77), Error message: " + e.toString());
+        }
+        return this;
+    }
+
+    public osthmFile setData(OsThmMetadata metadata) {
+        JSONObject tmp = null;
+        try {
+            // Extract the themesjson
+            tmp = data.getJSONObject("themesjson");
+        } catch (JSONException ignored) {}
+
+        // Then replace the data with metadata
+        data = new JSONObject(metadata.toHashMap());
+
+        // And put back the themesjson if themesjson exists
+        if (tmp != null) {
+            try {
+                data.put("themesjson", tmp);
+            } catch (JSONException e) {
+                // ..oops something went wrong
+                Log.e("osthFile", "Failed to put back themesjson while setting data as OsThmMetadata");
+            }
+        }
+
+        return this;
+    }
     // ===================================================================
 
 
     // ===================================================================
     public HashMap<String, Object> toHashMap() {
+        if (data != null)
+            return new Gson().fromJson(
+                    data.toString(), new TypeToken<HashMap<String, Object>>() {}.getType()
+            );
+
         return null;
     }
 
@@ -81,7 +128,45 @@ public class osthmFile {
 
         return null;
     }
+
+    public OsThmTheme toOsThmTheme() {
+        // Check if the data has been set by the user
+        if (data != null)
+            try {
+                return new OsThmTheme(data.getString("themesjson"));
+            } catch (JSONException e) {
+                // ..what? the user probably inserted an invalid data, return null instead
+                return null;
+            }
+
+        // Return null
+        return null;
+    }
+
+    public OsThmMetadata toOsThmMetadata() {
+        // Check if the data has been set by the user
+        if (data != null)
+            return new OsThmMetadata(this.toHashMap());
+
+        // Return null
+        return null;
+    }
     // ===================================================================
+
+    // Check if JSONObject is valid as osthm
+    private boolean isJSONValidAsOsThm(JSONObject json) {
+        try {
+            new OsThmTheme((HashMap<String, Integer>) new Gson().fromJson(
+                    json.getString("themesjson").toString(), new TypeToken<HashMap<String, Object>>() {}.getType()
+            ));
+            new OsThmMetadata((HashMap<String, Object>) new Gson().fromJson(
+                    json.toString(), new TypeToken<HashMap<String, Object>>() {}.getType()
+            ));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
     // Returns boolean indicating if the writing was successful or not
     public boolean write(int type) {
@@ -92,6 +177,10 @@ public class osthmFile {
         // Check if file is null
         if (file == null)
             throw new RuntimeException(new osthmException("File has not been set yet"));
+
+        // Check if the JSON is valid as osthm
+        if (!isJSONValidAsOsThm(data))
+            throw new RuntimeException(new osthmException("Invalid JSON data"));
 
         // Check the type
         if (type == osthmFile.OSTHM) {
