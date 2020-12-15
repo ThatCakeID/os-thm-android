9f79cce47f5421707113eea09a4b4bb38a347c63
Iyxan23 <nurihsanalghifari@gmail.com>
2020-12-15 13:26:58 +0700

    Some unfinished changes

--- lib/src/main/java/tw/osthm/osthmFile.java
+++ lib/src/main/java/tw/osthm/osthmFile.java
@@ -1,5 +1,6 @@
 package tw.osthm;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
@@ -7,7 +8,7 @@ import java.util.HashMap;
 
 public class osthmFile {
 
-    private String data;  // Raw JSON Data
+    private JSONObject data;  // Raw JSON Data
     private File file;
 
     // Types used in the function write()
@@ -20,30 +21,79 @@ public class osthmFile {
         this.file = file;
     }
 
-    public osthmFile setData(String data) {
-        this.data = data;
-        return null;
+    // ===================================================================
+    public osthmFile setFile(File file) {
+        this.file = file;
+        return this;
+    }
+
+    public osthmFile setFilePath(String filePath) {
+        this.file = new File(filePath);
+        return this;
+    }
+    // ===================================================================
+
+
+    // ===================================================================
+    public osthmFile setData(String data) throws JSONException {
+        this.data = new JSONObject(data);
+        return this;
     }
 
     public osthmFile setData(JSONObject data) {
-        // this.data = data;
-        return null;
+        this.data = data;
+        return this;
     }
 
     public osthmFile setData(HashMap<String, Object> data) {
-        // this.data = data;
-        return null;
+        this.data = new JSONObject(data);
+        return this;
     }
+    // ===================================================================
 
+
+    // ===================================================================
     public HashMap<String, Object> toHashMap() {
         return null;
     }
 
     public String toJsonString() {
-        return data;
+        if (data != null)
+            return data.toString();
+
+        return null;
     }
 
-    public void write(int type) {
+    public JSONObject toJSONObject() {
+        if (data != null)
+            return data;
 
+        return null;
+    }
+    // ===================================================================
+
+    // Returns boolean indicating if the writing was successful or not
+    public boolean write(int type) {
+        // Check if data is null
+        if (data == null)
+            throw new RuntimeException(new osthmException("Data has not been set yet"));
+
+        // Check if file is null
+        if (file == null)
+            throw new RuntimeException(new osthmException("File has not been set yet"));
+
+        // Check the type
+        if (type == osthmFile.OSTHM) {
+            return write_osthm();
+        } else if (type == osthmFile.RAW_JSON) {
+            return StorageUtil.createFile(file.getAbsolutePath(), data.toString());
+        } else {
+            throw new RuntimeException(new osthmException("Type argument is not valid, it should be osthmFile.OSTHM or osthmFile.RAW_JSON"));
+        }
+    }
+
+    private boolean write_osthm() {
+
+        return true;
     }
 }
